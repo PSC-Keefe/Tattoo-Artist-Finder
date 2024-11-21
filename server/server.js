73fceb08eb5c2d3b4d23b189f6d3216c7861eda0
TK <162350265+PSC-Keefe@users.noreply.github.com>
2024-11-20 20:35:45 -0500

    Update server.js
    Updated //Route to serve index.html by removing "/client/public" from the file path

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ db.connect(err => {
 
 // Route to serve index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Handle search request (search endpoint)
